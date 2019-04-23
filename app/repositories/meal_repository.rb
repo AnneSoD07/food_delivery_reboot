253ec0afd790f840a81c1e3d3fdcb8183c7a2783
Charles Pernet <charlespernet@gmail.com>
2019-04-23 17:53:38 +0200

    Meal repo done

---
+++ app/repositories/meal_repository.rb
@@ -0,0 +1,44 @@
+class MealRepository
+  def initialize(csv_file)
+    @meals = []
+    @csv_file = csv_file
+    load_csv if File.exist?(@csv_file)
+    @next_id = @meals.empty? ? 1 : @meals.last.id + 1
+  end
+
+  def all
+    @meals
+  end
+
+  def add(meal)
+    meal.id = @next_id
+    @next_id += 1
+    @meals << meal
+    save_csv
+  end
+
+  def find(id)
+    @meals.find { |meal| meal.id == id }
+  end
+
+  private
+
+  def load_csv
+    csv_options = { headers: :first_row, header_converters: :symbol }
+    CSV.foreach(@csv_file, csv_options) do |row|
+      row[:id] = row[:id].to_i
+      row[:price] = row[:price].to_i
+
+      @meals << Meal.new(row)
+    end
+  end
+
+  def save_csv
+    CSV.open(@csv_file, 'wb') do |csv|
+      csv << %w(id name price)
+      @meals.each do |meal|
+        csv << [meal.id, meal.name, meal.price]
+      end
+    end
+  end
+end
